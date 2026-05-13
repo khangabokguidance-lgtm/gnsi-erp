dc524e7a3f1872ddf9ed9063ab3bf9a5e54bb56c
khangabokguidance-lgtm <khangabokguidance@gmail.com>
2026-05-13 14:39:46 +0530

    Fresh GNSI ERP - clean start

--- src/core/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://hiqaqdfhopuakaydfkgb.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpcWFxZGZob3B1YWtheWRma2diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1Mzc4MzMsImV4cCI6MjA5MDExMzgzM30.kJ7dL57alviRjOLc0BsEk9eS_90wwQahvQfYD2GLZ68'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
